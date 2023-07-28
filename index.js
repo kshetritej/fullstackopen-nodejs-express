40a57ad9bed658d67882dacafd0848f8ea930f83
kshetritej <tejghartikshetri@gmail.com>
2023-07-28 19:05:42 +0545

    uibuild

--- index.js
+++ index.js
@@ -11,90 +11,30 @@ morgan.token('type', function (req, res) { return JSON.stringify(persons[persons
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :type'))
 
+//mongoose definations
+const mongoose = require('mongoose')
 
-let persons = [
-    {
-        id: 1,
-        name: 'Arto Hellas',
-        number: '040-123456', 
-    },
-    {
-        id: 2,
-        name: 'Ada Lovelace',
-        number: '39-44-53525252',
-    },
-    {
-        id: 3,
-        name: 'Dan Abramov',
-        number: '12-43-25252',
-    },
-    {
-        id: 4,
-        name: 'Mary Poppendick',
-        number: '39-23-5252',
-    }
-]
+const password = process.argv[2]
 
-const date = new Date()
+const url = `mongodb+srv://kshetritej:${password}@cluster0.yg8tfaf.mongodb.net/Phonebook?retryWrites=true&w=majority`
 
-// root directory (localhost:3001)
-// app.get('/', (req, res) => {
-//     res.json()
-// })
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
 
-//for displaying all results
-app.get('/api/persons', (req, res) => {
-    res.json(persons)
+const phonebookSchema = new mongoose.Schema({
+    id: Number,
+    name : String,
+    number: String,
 })
 
-//for displaying info page
-app.get('/info', (req, res) => {
-    res.send(` <p>Phonebook has info for ${persons.length} people <br/> ${date}</p>`)
+const Phonebook = mongoose.model('Contact',phonebookSchema)
+
+app.get('/api/persons', (req,res) => {
+    Person.find({}).then(person =>{
+        res.json(persons)
+    })
 })
 
-//For displaying single phonebook entry
-app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const person = persons.find(person => person.id === id)
-    if (person) {
-        res.json(person)
-    }
-    res.status(404).end()
-})
-
-//For deleting data 
-app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    persons = persons.filter(person => person.id !== id)
-
-    res.status(204).end()
-})
-
-//for posting data
-app.post('/api/persons', (req, res) => {
-    const body = req.body
-
-    const person = {
-        id: Math.floor((Math.random() * 10)),
-        name: "Hell's Kitchen",
-        number: "080580285"
-    }
-    if (!person.name) {
-        return res.status(400).json({
-            error: 'content missing'
-        })
-    }
-    
-    const findName = persons.map(person => person.name)
-    if(findName.includes(person.name)){
-        return res.status(400).json({
-            error: 'name must be unique'
-        })
-    }
-
-    persons = persons.concat(person)
-    res.json(person)
-})
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
